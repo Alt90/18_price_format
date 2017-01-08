e7b325009e020f0b6adbfac5a116d1a81a1fbd0c
Alt13 <Alt90@rambler.ru>
2017-01-08 15:12:07 +0300

    Запилил тесты и функцию вывода цены

--- format_price.py
+++ format_price.py
@@ -1,5 +1,24 @@
+import argparse
+
+
+def get_args():
+    parser = argparse.ArgumentParser(
+        description="This script can change price to format",
+    )
+    parser.add_argument('--price')
+    return parser.parse_args()
+
+
 def format_price(price):
-    pass
+    if price is None:
+        return ''
+    price = float(str(price).replace(',', '.'))  
+    if price.is_integer():
+        return '{:,.0f}'.format(float(price)).replace(',', ' ')
+    else:
+        return '{:,.2f}'.format(float(price)).replace(',', ' ')
+
 
 if __name__ == '__main__':
-    pass
+    args = get_args()
+    print(format_price(args.price))
--- tests.py
+++ tests.py
@@ -1,1 +1,45 @@
-# TODO place your tests here
+import unittest
+
+from format_price import format_price
+
+
+class PriceTestCase(unittest.TestCase):
+    def test_none_price(self):
+        price = format_price(None)
+        self.assertEqual(price, '')
+
+    def test_int_price(self):
+        price = format_price(4323)
+        self.assertEqual(price, '4 323')
+
+    def test_int_price_with_delimiter(self):
+        price = format_price(4672.00)
+        self.assertEqual(price, '4 672')
+
+    def test_float_price(self):
+        price = format_price(4672.03567)
+        self.assertEqual(price, '4 672.04')
+
+    def test_str_price_with_delimiter(self):
+        price = format_price('25694,03567')
+        self.assertEqual(price, '25 694.04')
+
+    def test_str_price_with_rigth_delimiter(self):
+        price = format_price('25694.03567')
+        self.assertEqual(price, '25 694.04')
+
+    def test_str_price(self):
+        price = format_price('2569')
+        self.assertEqual(price, '2 569')
+
+    def test_str_price_in_right_exp_format(self):
+        price = format_price('1.2354e+4')
+        self.assertEqual(price, '12 354')
+
+    def test_str_price_in_exp_format(self):
+        price = format_price('1,2354453E+4')
+        self.assertEqual(price, '12 354.45')
+
+
+if __name__ == '__main__':
+    unittest.main()
